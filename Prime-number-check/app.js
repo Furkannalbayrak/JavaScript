0cd4784523e344038189fcdf9bab39c3bf875583
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-02-22 19:42:45 +0300

    Add files via upload

---
+++ Prime-number-check/app.js
@@ -0,0 +1,17 @@
+// ------------- ASAL SAYİ BULMA --------------
+
+let sayi = Number(prompt("Lütfen bir sayi giriniz : "));
+let toplam = 0;
+
+for(let a = 2;a < sayi/2;a++){
+    if(sayi % a == 0){
+        toplam++;
+        break;
+    }
+}
+if(toplam == 0){
+    alert(sayi + " asal bir sayidir");
+}
+else{
+    alert(sayi + " asal bir sayi değildir");
+}
