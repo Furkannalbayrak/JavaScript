8e874572b0f06689639ebba9846b2ac774655cdf
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-02-27 00:05:25 +0300

    Update app.js

--- Armstrong-number-check/app.js
+++ Armstrong-number-check/app.js
@@ -17,23 +17,3 @@ if (toplam == sayi2) {
 else {
     alert(sayi2 + " amstrong sayisi degildir");
 }
-
-
-// 2.yöntem
-
-/*
-let sayi = prompt("Lütfen bir sayi giriniz : ");
-let rakam;
-let toplam = 0;
-
-for(let i = 0;i < sayi.length;i++){  // sayi.length numberde çalışmaz stringde çalışır
-    rakam = sayi.charAt(i);  // sayi.charAt(2) demek sayinin 3. rakamı (dizi gibi ilk rakam 0 dır)
-    toplam += rakam**3;
-}
-if(toplam == sayi){
-    alert(sayi + " amstrong sayisidir");
-}
-else{
-    alert(sayi + " amstrong sayisi degildir");
-}
-*/
