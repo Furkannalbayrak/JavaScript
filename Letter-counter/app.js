54ad5180a2a826eb13ac0333a471a2c07551884c
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-03-03 21:28:53 +0300

    Add files via upload

---
+++ Letter-counter/app.js
@@ -0,0 +1,27 @@
+// ------------ METİNDEKİ HARF SAYİSİNİ BULMA -------------
+
+let metin = prompt("Girmek istediğiniz metni giriniz: ");
+
+let harf = prompt("Lütfen bir harf giriniz");
+
+function bul(harf) {
+    let toplam = 0;
+
+    for (let i = 0; i < metin.length; i++) {
+        if (harf == metin.charAt(i)) {
+            toplam++;
+        }
+    }
+    return toplam;
+}
+
+let sonuc = bul(harf);
+alert("Metinde " + sonuc + " tane " + harf + " harfi bulunmaktadır")
+
+
+
+
+
+
+
+
