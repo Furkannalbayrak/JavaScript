81fad5b5f9ec44ea3533781c16a707692ae290c1
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-03-11 17:23:52 +0300

    Update app.js

--- Decimal-to-binary-converter/app.js
+++ Decimal-to-binary-converter/app.js
@@ -1,4 +1,3 @@
-// ------------- 10'LUK SAYİYİ 2'LİGE CEVİRME --------------
 
 let sayi = Number(prompt("Lütfen bir sayi giriniz : "));
 function convert(sayi){
@@ -6,9 +5,6 @@ function convert(sayi){
 
     while(sayi != 0){
         metin =  metin + String((sayi % 2));
-        // ! NOT: metin = String((sayi % 2)) + metin;
-        // ! bu şekilde yapsaydık stringi döndürmeye gerek kalmıycaktı.
-        // ! stringe elemanlar sondan eklenicekti.
         sayi = Math.floor(sayi / 2);
     }
     let sonuc = tersCevir(metin);
@@ -24,3 +20,4 @@ function tersCevir(metin){
 
     return metinTers;
 }
+
