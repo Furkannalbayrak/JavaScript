fd352f8019143470da6db76639dfcb7e50f22124
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-06-15 14:00:30 +0300

    Update currency.js

--- Currency-converter/currency.js
+++ Currency-converter/currency.js
@@ -1,7 +1,10 @@
 
+//⚠️ Not: Bu proje Freecurrency API kullanmaktadır. Kendi kullanımınız için [Free Currency API](https://freecurrencyapi.com/) sayfasından ücretsiz bir API anahtarı
+// alarak `currency.js` dosyasındaki `YOUR_API_KEY` kısmına kendi anahtarınızı eklemeniz gerekmektedir.
+
 class Currency{
     constructor(){
-        this.url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_eFhJztXAABv9hzv38B0z2VJImC8KBvQ4m5YeP1Ae&base_currency="
+        this.url = "https://api.freecurrencyapi.com/v1/latest?apikey=YOUR_API_KEY&base_currency="
     }
 
     async exchange(amount, firstCurrency, secondCurrency){
