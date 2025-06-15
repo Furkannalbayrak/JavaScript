0975d8371ec2d3308a0dd492866b83ffd5efdda6
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-06-15 13:43:29 +0300

    Add files via upload

---
+++ Movie-search/app.js
@@ -0,0 +1,91 @@
+
+const getMovie = document.querySelector(".getMovie");
+const form = document.querySelector(".form");
+const container2 = document.querySelector(".container2");
+const showMore = document.querySelectorAll(".showDetails");
+
+runEventListeners();
+
+function runEventListeners(){
+    form.addEventListener("submit", listMovie);
+    container2.addEventListener("click", (e) =>{
+        if(e.target.classList == "showDetails"){
+            showDetails(e);
+        }
+    })
+
+//     container2.addEventListener("click", (e) =>{      contains ile yapma
+//         if(e.target.classList.contains("showDetails")){
+//             showDetails(e);
+//         }
+//     })
+}
+
+function showDetails(e){
+    const value = e.target.parentElement.children[0].textContent.trim();
+    sessionStorage.setItem("value", value);
+    window.open("index2.html", "_blank");
+}
+
+async function listMovie(e){
+    e.preventDefault();
+    const value = getMovie.value.trim();
+    const myKey = "d8b2f19f";
+    if(value == ""){
+        alert("Boş bırakmayınız");
+        return;
+    }
+
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${myKey}&s=${value}`);
+    if(!response.ok){
+        alert("İstek basarisiz oldu");
+        return;
+    }
+    const data = await response.json();
+    if(data.Response == "False"){
+        alert("Film bulunamadi");
+        return;
+    }
+
+    clearListMovie();
+
+    data.Search.forEach(element => {
+        if(element.Poster != "N/A"){
+            addNewMovie(element.Title, element.Poster);
+        }
+    });
+    
+    getMovie.value = "";
+}
+
+function clearListMovie(){
+    Array.from(container2.children).forEach((element)=>{
+        element.remove();
+    })
+}
+
+function addNewMovie(name, poster){
+    const div = document.createElement("div");
+    div.classList = "container2Childs";
+
+    const div2 = document.createElement("div");
+    div2.classList = "container2Child";
+
+    const img = document.createElement("img");
+    img.classList = "picture";
+    img.title = name;
+    img.src = poster;
+
+    const p = document.createElement("p");
+    p.textContent = name;
+
+    const button = document.createElement("button");
+    button.classList = "showDetails";
+    button.textContent = "Daha Detayli Görüntüle";
+
+    div.appendChild(img);
+    div.appendChild(p);
+    div2.appendChild(div);
+    div2.appendChild(button);
+    container2.appendChild(div2);
+}
---
+++ Movie-search/app2.js
@@ -0,0 +1,77 @@
+
+const container = document.querySelector(".container");
+
+runEventListener();
+
+async function runEventListener(){
+    const value = sessionStorage.getItem("value");
+    const myKey = "d8b2f19f";
+
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${myKey}&t=${value}`);
+    if(!response.ok){
+        alert("İstek basarisiz oldu");
+        return;
+    }
+    const data = await response.json();
+
+    addNewDetails(data);
+}
+
+function addNewDetails(data){
+    const img = document.createElement("img");
+    img.src = data.Poster;
+
+    const div1 = document.createElement("div");
+    div1.classList = "containerChild1";
+
+    const div2 = document.createElement("div");
+    div2.classList = "containerChild2";
+
+    const p = document.createElement("p");
+    p.classList = "abstract";
+    p.textContent = data.Plot;
+
+    const ul = document.createElement("ul");
+    ul.classList = "list";
+
+    const li1 = document.createElement("li");
+    const li2 = document.createElement("li");
+    const li3 = document.createElement("li");
+    const li4 = document.createElement("li");
+    const li5 = document.createElement("li");
+    const li6 = document.createElement("li");
+    const li7 = document.createElement("li");
+    const li8 = document.createElement("li");
+    const li9 = document.createElement("li");
+    const li10 = document.createElement("li");
+
+    li1.textContent = `Film Adı : ${data.Title}`;
+    li2.textContent = `Yıl : ${data.Year}`;
+    li3.textContent = `Süre : ${data.Runtime}`;
+    li4.textContent = `Kategori : ${data.Genre}`;
+    li5.textContent = `Yönetmen : ${data.Director}`;
+    li6.textContent = `Yazar : ${data.Writer}`;
+    li7.textContent = `IMDB : ${data.imdbRating}`;
+    li8.textContent = `Oyuncular : ${data.Actors}`;
+    li9.textContent = `Ülke : ${data.Country}`;
+    li10.textContent = `Tür : ${data.Type}`;
+
+    ul.appendChild(li1);
+    ul.appendChild(li7);
+    ul.appendChild(li3);
+    ul.appendChild(li2);
+    ul.appendChild(li5);
+    ul.appendChild(li4);
+    ul.appendChild(li8);
+    ul.appendChild(li9);
+    ul.appendChild(li6);
+    ul.appendChild(li10);
+
+    div1.appendChild(img);
+    div2.appendChild(p);
+    div2.appendChild(ul);
+
+    container.appendChild(div1);
+    container.appendChild(div2);
+
+}
